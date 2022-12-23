99210530b43b515980477f096a8e8df93f45e01a
Nataliya Kim <magistr1982@gmail.com>
2022-12-23 14:52:35 +0200

    Add slider animation

---
+++ src/js/slider.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 2000,
+    easing: 'ease',
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    swipe: true,
+
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
